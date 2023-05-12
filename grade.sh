4ea23805c08b744a56e52421fe7b97284a15a241
vlmontegrande <vlmontegrande@gmail.com>
2023-05-11 19:59:06 -0700

    Finished

--- grade.sh
+++ grade.sh
@@ -14,3 +14,24 @@ echo 'Finished cloning'
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
+
+if [[ -f student-submission/ListExamples.java ]] 
+then
+    cp student-submission/ListExamples.java grading-area
+    cp TestListExamples.java grading-area
+else
+    echo 'ListExamples.java not found'
+    exit
+fi
+
+javac grading-area/ListExamples.java
+if [[ $? -ne 0 ]] 
+then
+    echo 'ListExamples.java failed to compile'
+    exit
+fi
+
+cd grading-area
+
+javac -cp ".;../lib/hamcrest-core-1.3.jar;../lib/junit-4.13.2.jar" *.java
+java -cp ".;../lib/junit-4.13.2.jar;../lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore TestListExamples
